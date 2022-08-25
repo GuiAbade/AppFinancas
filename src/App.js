503971451f9703d18c25af642047f1fd7e39d253
GuiAbade <abade.guim13@gmail.com>
2022-08-25 20:25:07 -0300

    Estruturação do ResumeItems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
-import GlobalStyle from './styles/global'
+import Resume from './components/Resume';
+import GlobalStyle from './styles/global';
 
 const App = () => {
   return (
     <>
       <Header/>
+      <Resume/>
       <GlobalStyle/>
     </>
   );
