1a5152fbeb36a6f3e2e224103858b573ee0b4ab4
GuiAbade <abade.guim13@gmail.com>
2022-09-09 18:34:47 -0300

    Adicionando resumo

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ResumeItem from '../ResumeItem';
 import * as C from './styles';
+  import{
+    FaAngleUp,
+    FaAngleDown,
+    FaDollarSign,
+  } from 'react-icons/fa';
 
 const Resume = () => {
   return (
     <C.Container>
-      <ResumeItem/>
-      <ResumeItem/>
-      <ResumeItem/>
+      <ResumeItem title="Entradas" Icon={FaAngleUp} value="1000" />
+      <ResumeItem title="Saídas" Icon={FaAngleDown} value="1000" />
+      <ResumeItem title="Total" Icon={FaDollarSign} value="1000" />
     </C.Container>
   );
 };
